e9f2b51992a549c499e4726c659b67a22a3ea909
Ivan Ivanov <ivanov.biol@gmail.com>
2021-12-22 10:37:06 +0200

    Change github token in GitHubConfig

--- src/main/java/com/company/addonis/config/GitHubConfig.java
+++ src/main/java/com/company/addonis/config/GitHubConfig.java
@@ -12,7 +12,7 @@ public class GitHubConfig {
 
     public static final String GITHUB_CONNECTION_ERROR_MESSAGE = "Unable to connect GitHub. " +
             "Please check if your token is valid.";
-    public static final String GITHUB_TOKEN_LINK = "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";
+    public static final String GITHUB_TOKEN_LINK = "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";
 
     public GitHub gitHubTokenLink() {
 
