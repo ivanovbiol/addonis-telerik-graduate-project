fcb8e5ff0ef00c8018466aeaea3354bb2fc67ac7
Ivan Ivanov <ivanov.biol@gmail.com>
2021-12-23 11:26:09 +0200

    Refactor error messages in UserPasswordChangeDto

--- src/main/java/com/company/addonis/models/dtos/UserPasswordChangeDto.java
+++ src/main/java/com/company/addonis/models/dtos/UserPasswordChangeDto.java
@@ -5,13 +5,22 @@ import javax.validation.constraints.Size;
 
 public class UserPasswordChangeDto {
 
+    @Size(min = 8, message = "Password must have at least 8 symbols.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$",
+            message = "Password must be at least 8 characters long " +
+                    "with 1 uppercase, 1 lowercase 1, special character & 1 numeric character.")
     private String oldPassword;
 
     @Size(min = 8, message = "Password must have at least 8 symbols.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$", message = "password must be at least 8 characters long " +
-            "with 1 uppercase, 1 lowercase 1, special character & 1 numeric character.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$",
+            message = "Password must be at least 8 characters long " +
+                    "with 1 uppercase, 1 lowercase 1, special character & 1 numeric character.")
     private String newPassword;
 
+    @Size(min = 8, message = "Password must have at least 8 symbols.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$",
+            message = "Password must be at least 8 characters long " +
+                    "with 1 uppercase, 1 lowercase 1, special character & 1 numeric character.")
     private String confirmNewPassword;
 
     public UserPasswordChangeDto() {
